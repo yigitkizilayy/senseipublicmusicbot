ac39dd217ae10385634280883804c92a3e1f7769
Glitch (boiled-observation) <none>
2019-04-06 21:55:16 +0000

    🚞⛺️ Checkpoint ./komutlar/yardım.js:1934803/114 ./komutlar/mdevamet.js:1934803/1432

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,10 +10,10 @@ exports.run = (client, message, params) => {
   .setColor('RANDOM')
   .setTitle('```Techno Müzik Bot Komutları```')
   .addField(`:robot: Bot Komutları :robot:`, '!prefix: `botun prefixini değiştirebilirsiniz.` \n!davet: `botu davet edebilirsiniz.`')
-  .addField(`:musical_note: Müzik Komutları :musical_note:`, '!çal: `İstediğin şarkıyı çalar.`\n!duraklat: `Müziği duraklatır.`\n!devam: `Müziği devam ettirir.`\n!tekrar:` Müziği tekrarlatır.`\n!geç: `Geçerli çalınan müziği geçer`\n!stop: `Müziği kapatır.`')
+  .addField(`:musical_note: Müzik Komutları :musical_note:`, '!çal: `İstediğin şarkıyı çalar.`\n!duraklat: `Müziği duraklatır.`\n!devamet: `Müziği devam ettirir.`\n!tekrar:` Müziği tekrarlatır.`\n!geç: `Geçerli çalınan müziği geçer`\n!dur: `Müziği kapatır.`')
  .addField(`------------------`, '-')
-  .addField(`:robot: Bot Command :robot:`, '!prefix: `Changes prefix.` \n!davet: `İnvite the bot`')
-  .addField(`:musical_note: Music Commands :musical_note:`, '!play: `playing to music`. \npauses: `stop music.` \ncontinuation: `continues music.`')
+  .addField(`:robot: Bot Commands :robot:`, '!prefix: `Changes prefix.` \n!davet: `İnvite the bot`')
+  .addField(`:musical_note: Music Commands :musical_note:`, '!play: `playing to music`. \n!pauses: `stop music.` \n!continuation: `continues music.` \n!again: `repeats music` \n!passes: `passes music` \n!stop: `stop the music`')
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
