8c9d3b8549d0d5fa6e23c517ea3af80b8fe43b4b
Glitch (boiled-observation) <none>
2019-04-07 08:55:34 +0000

    🌯📮 Checkpoint ./komutlar/otorolqç.js:1934803/1928 ./komutlar/otorolkapa.js:1934803/1125 ./komutlar/otorolmesajkapatma.js:1934803/1152 ./server.js:1934803/1218 ./bot.js:1934803/1222 ./otorol.json:1934803/2

--- bot.js
+++ bot.js
@@ -133,6 +133,42 @@ client.on('error', e => {
 });
 
 
+
+client.on("guildMemberAdd", async member => {
+        let sayac = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
+  let otorole =  JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
+      let arole = otorole[member.guild.id].sayi
+  let giriscikis = JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
+  let embed = new Discord.RichEmbed()
+    .setTitle('Otorol Sistemi')
+    .setDescription(`:loudspeaker: :inbox_tray:  @${member.user.tag}'a Otorol Verildi `)
+.setColor("GREEN")
+    .setFooter("Harmony ", client.user.avatarURL);
+
+  if (!giriscikis[member.guild.id].kanal) {
+    return;
+  }
+
+  try {
+    let giriscikiskanalID = giriscikis[member.guild.id].kanal;
+    let giriscikiskanali = client.guilds.get(member.guild.id).channels.get(giriscikiskanalID);
+    giriscikiskanali.send(`:loudspeaker: :white_check_mark: Hoşgeldin **${member.user.tag}** Rolün Başarıyla Verildi.`);
+  } catch (e) { // eğer hata olursa bu hatayı öğrenmek için hatayı konsola gönderelim.
+    return console.log(e)
+  }
+
+});
+
+client.on("guildMemberAdd", async (member) => {
+      let autorole =  JSON.parse(fs.readFileSync("./otorol.json", "utf8"));
+      let role = autorole[member.guild.id].sayi
+
+      member.addRole(role)
+});
+
+
+
+
 /*client.on("guildMemberAdd", async member => {
          let anan = member.user.avatarURL || member.user.defaultAvatarURL
     let memberChannel = await db.fetch(`memberChannel_${member.guild.id}`)
