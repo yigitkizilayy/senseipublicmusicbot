e8ab02a21d107c67ae3fd3ee1ca9946507fc7bd3
Glitch (boiled-observation) <none>
2019-04-07 09:15:41 +0000

    🎖👘 Checkpoint ./komutlar/davettakip.js:1934803/869 ./server.js:1934803/759

---
+++ komutlar/davettakip.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+exports.run = async (client ,message, args) =>{
+
+
+   let logkanali = message.mentions.channels.first();
+   if (!logkanali) message.channel.send('Bir Log Kanalı Belirlemelisin')
+
+   db.set(`davetk_${message.guild.id}`, message.mentions.channels.first().id).then(i => {
+
+    message.channel.send(`:white_check_mark: Davet kanalı Başarıyla <#${i}> olarak ayarlandı.`)    
+   })
+};
+exports.conf = {
+ enabled: true,
+ guildOnly: false,
+ aliases: ['davet-kanal-ayarla'],
+ permLevel: 0
+};
+
+exports.help = {
+ name: 'davet-kanalı-ayarla',
+ description: 'Davet Log Kanalını Belirler',
+ usage: 'davet-kanal-ayarla #kanal'
+};
+
+//ttps://hastebin.com/inadaneros.js
+
+
+//Bot.js'e Atıl
