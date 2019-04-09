53b6a8a19b636b5ca3db20c7303bcefaddebc0cb
Glitch (boiled-observation) <none>
2019-04-09 04:58:39 +0000

    🏈😬 Checkpoint ./server.js:1934803/2893 ./events/ready.js:1934803/2054

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,3 @@
-//events/ready.js
 const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
@@ -6,14 +5,27 @@ const ayarlar = require('../ayarlar.json');
 
 var prefix = ayarlar.prefix;
 
-
 module.exports = client => {
-  console.log(`BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("");
-  client.user.setGame(`${prefix}yardım 》`+ client.guilds.size +` Sunucu | Tekrardan Hizmet Veriyoruz.`);
-  console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-};
-//XiR
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+   var oyun = [
+       // "Destek sunucumuz saldırıya uğradı sunucumuza gelmek için g!desteksunucum yazabilirsiniz",
+       // "Destek sunucumuz saldırıya uğradı sunucumuza gelmek için g!desteksunucum yazabilirsiniz",
+        "💪 7/24 Aktif!",  
+        "💡 !davet | İle Botumuzu ekleyin",
+        "👨 2 bin kullanıcı",
+        "🌍 81 Sunucuda Hizmet!",
+        "!yardım 🔥 + !davet "
+     Gelişmiş ready.js
+client.user.setGame(`${prefix}yardım | ${client.guilds.size} sunucu`);
+     
+    ];
 
+    setInterval(function() {
 
-//idle bölümünü dnd - online - dnd herhangi birisi ile değiştirirseniz botun çevrimiçi durumunu değiştirebilirsiniz..
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/erenbaltaci_");
+        }, 2 * 2500);
+}
--- server.js
+++ server.js
@@ -114,6 +114,79 @@ let gc = JSON.parse(fs.readFileSync("./log.json", "utf8"));
 
 
 
+client.on('message', msg => {
+  const reason = msg.content.split(" ").slice(1).join(" ");
+  if (msg.channel.name== 'destek') { 
+    const hatay = new Discord.RichEmbed()
+    .addField(" Hata ", `Bu Sunucuda \`Destek\` Adında Bir Rol Yok!`)
+    .setColor("RANDOM")
+    
+    if (!msg.guild.roles.exists("name", "Destek")) return msg.author.send(hatay) + msg.guild.owner.send(`${msg.guild.name} Adlı Sunucunda, \`Destek\` Adlı Bir Rol Olmadığı İçin, Hiçkimse Destek Talebi Açamıyor!`);
+    if(msg.guild.channels.find('name', 'Talepler')) {
+      msg.guild.createChannel(`destek-${msg.author.id}`, "text").then(c => {
+      const category = msg.guild.channels.find('name', 'Talepler')
+      c.setParent(category.id)
+      let role = msg.guild.roles.find("name", "Destek");
+      let role2 = msg.guild.roles.find("name", "@everyone");
+      c.overwritePermissions(role, {
+          SEND_MESSAGES: false,
+          READ_MESSAGES: false
+      });
+      c.overwritePermissions(role2, {
+          SEND_MESSAGES: false,
+          READ_MESSAGES: false
+      });
+      c.overwritePermissions(msg.author, {
+          SEND_MESSAGES: true,
+          READ_MESSAGES: true
+      });
+
+      const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setAuthor(`${client.user.username} | Destek Sistemi`)
+      .addField(`Merhaba ${msg.author.username}!`, `Destek Yetkilileri burada seninle ilgilenecektir. \nDestek talebini kapatmak için \`${prefix}kapat\` yazabilirsin.`)
+      .addField(`» Talep Konusu/Sebebi:`, `${msg.content}`, true)
+      .addField(`» Kullanıcı:`, `<@${msg.author.id}>`, true)
+      .setFooter(`${client.user.username} | Destek Sistemi`)
+      .setTimestamp()
+      c.send({ embed: embed });
+      c.send(`<@${msg.author.id}> Adlı kullanıcı "\`${msg.content}\`" sebebi ile destek talebi açtı! Lütfen Destek Ekibini bekle, @here`)
+      msg.delete()
+      }).catch(console.error);
+    }
+  }
+});
+  
+client.on("message", message => {
+if (message.content.toLowerCase().startsWith(prefix + `kapat`)) {
+    if (!message.channel.name.startsWith(`destek-`)) return message.channel.send(`Bu komut sadece Destek Talebi kanallarında kullanılablir!`);
+
+    var deneme = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`Destek Talebi Kapatma İşlemi`)
+    .setDescription(`Destek talebini kapatmayı onaylamak için, \n10 saniye içinde \`evet\` yazınız.`)
+    .setFooter(`${client.user.username} | Destek Sistemi`)
+    message.channel.send(deneme)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === 'evet', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Destek Talebi kapatma isteğin zaman aşımına uğradı!').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+}
+});
+
+
+
 
 
 
