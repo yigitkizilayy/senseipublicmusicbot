0265f148c5034e32bb26a15966ca30a0d1828991
Glitch (boiled-observation) <none>
2019-04-10 19:56:23 +0000

    😬🗽 Checkpoint ./komutlar/davettakip.js:1934803/777

--- komutlar/davettakip.js
+++ komutlar/davettakip.js
@@ -1,56 +1,30 @@
 const Discord = require('discord.js')
-const fs = require('fs');
-var ayarlar = require('../ayarlar.json');
-let kanal = JSON.parse(fs.readFileSync("././jsonlar/sKanal.json", "utf8"));
+const db = require('quick.db')
 
-exports.run = async (client, message, args) => {
-if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu komutu kullanabilmek için **Yönetici** iznine sahip olmalısın!`);
-  
-  const db = require('quick.db');
-  
-  /*var s = 'tr'
-  var a = client.commands.get('davet-kanal-ayarla').help.name
-    if(db.has(`dil_${message.guild.id}`) === true) {
-        var s = 'en'
-        var a = client.commands.get('davet-kanal-ayarla').help.enname
-    }
-    const dil = client[s]
-    const o = a
-  
-  let channel = message.mentions.channels.first()
-  
-    if (!channel) {
-        return message.reply(dil.argerror.replace("{prefix}", client.ayarlar.prefix).replace("{komut}", o))
-    }
+exports.run = async (client ,message, args) =>{
 
-    if(!kanal[message.guild.id]){
-        kanal[message.guild.id] = {
-            sayacKanal: channel.id
-        };
-    }
-    */
-  
-    fs.writeFile("././jsonlar/sKanal.json", JSON.stringify(kanal), (x) => {
-        if (x) console.error(x)
-      })
-  
-    db.set(`dKanal_${message.guild.id}`, "<#"+channel.id+">")
-  
-    const embed = new Discord.RichEmbed()
-    .setDescription(`${dil.basarili}: ${channel}`)
-    .setColor("RANDOM")
-    message.channel.send({embed})
-}
-    
+
+   let logkanali = message.mentions.channels.first();
+   if (!logkanali) message.channel.send('Bir Log Kanalı Belirlemelisin')
+
+   db.set(`davetk_${message.guild.id}`, message.mentions.channels.first().id).then(i => {
+
+    message.channel.send(`:white_check_mark: Davet kanalı Başarıyla <#${i}> olarak ayarlandı.`)    
+   })
+};
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['davettakip'],
-    permLevel: 4
-}
+ enabled: true,
+ guildOnly: false,
+ aliases: ['davettakip', 'dt'],
+ permLevel: 0
+};
 
 exports.help = {
-    name: 'davet-kanal-ayarla',
-    description: 'Davet kanalını ayarlar.',
-    usage: 'davet-kanal-ayarla <#kanal>'
-}
+ name: 'davet-kanalı-ayarla',
+ description: 'Davet Log Kanalını Belirler',
+ usage: 'davet-kanal-ayarla #kanal'
+};
+
+
+//omutlara Atılacaktır.
+//omut Alıntıdır.
