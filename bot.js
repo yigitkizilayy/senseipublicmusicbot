d6d6d4da9f3e2b005cda7b56197975c9ebaee1ac
Glitch (tartan-rain) <none>
2019-07-03 09:38:13 +0000

    👲🤑 Checkpoint ./bot.js:3190412/6

--- bot.js
+++ bot.js
@@ -148,7 +148,7 @@ client.on('message', async msg => {
 	let command = msg.content.toLowerCase().split(' ')[0];
 	command = command.slice(prefix.length)
 
-	if (command === 'çal') {
+	if (command === 'oynat') {
 		const voiceChannel = msg.member.voiceChannel;
 		if (!voiceChannel) return msg.channel.sendEmbed(new Discord.RichEmbed()
       .setColor('RED')
