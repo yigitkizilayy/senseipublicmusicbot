6785a7340ac33f889d094da15f6526771f9ea215
Glitch (tartan-rain) <none>
2019-07-03 09:27:13 +0000

    📈🚏 Checkpoint ./ayarlar.json:3190412/341 ./bot.js:3190412/14 ./package.json:3190412/53984

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ const app = express();
 app.get("/", (request, response) => {
   response.sendStatus(200);
     client.user.setStatus("dnd");
-  client.user.setActivity(`!yardım | Sizi`, { type: "LISTENING"});
+  client.user.setActivity(`?yardım | ?oynat`, { type: "LISTENING"});
 });
 app.listen(process.env.PORT);
 setInterval(() => {
