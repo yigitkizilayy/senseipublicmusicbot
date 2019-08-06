3ed88f24cb7556bea81185032d817f287ab35a4c
Glitch (tola-botum) <none>
2019-08-06 08:58:59 +0000

    ☺️💶 Checkpoint ./komutlar/yardım.js:3190412/127 ./bot.js:3190412/32

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const ayarlar = require('moment');
-const prefix = 'k!'
+const prefix = 'g!'
 
 exports.run = (client, message, params) => {
   const embedyardim = new Discord.RichEmbed()
   .setThumbnail(client.user.avatarURL)
-  .setDescription(`**═══════════════════════════════════════**\n\n<a:muzik:608207534695645184>\`?oynat\` = **Yazdığınız şarkıyı çalar.** \n <a:muzik:608207534695645184>\`?geç\` = **Dinlediğiniz Şarkıyı Geçer.** \n <a:muzik:608207534695645184>\`?durdur\` = **Şarkıyı Durdurur Ve Bot Odadan Ayrılır.** \n <a:muzik:608207534695645184>\`?duraklat\` = **Şarkıyı Durdurur**. \n <a:muzik:608207534695645184>\`?devamet\` = **Durdurulan Şarkı Devam Eder**. \n 💎\`g!ekstra\` = **Ekstra Komutları Gösterir**.  \n\n**═══════════════════════════════════════**`)
+  .setDescription(`**═══════════════════════════════════════**\n\n<a:muzik:608207534695645184>\`?oynat\` = **Yazdığınız şarkıyı çalar.** \n <a:muzik:608207534695645184>\`?geç\` = **Dinlediğiniz Şarkıyı Geçer.** \n <a:muzik:608207534695645184>\`?durdur\` = **Şarkıyı Durdurur Ve Bot Odadan Ayrılır.** \n <a:muzik:608207534695645184>\`?duraklat\` = **Şarkıyı Durdurur**. \n <a:muzik:608207534695645184>\`?devamet\` = **Durdurulan Şarkı Devam Eder**. \n <a:muzik:608207534695645184>\`g!davet\` = **Botun Davet Linkini Atar.**.  \n\n**═══════════════════════════════════════**`)
   .setColor(0x00ffff)
   .setTimestamp()
   .setFooter(`${message.author.username} ➠ Tarafından İstendi.`, message.author.avatarURL)
@@ -17,7 +17,7 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ['y', 'komutlar', 'help', 'h'],
-  permLevel: 4
+  permLevel: 0
 };
 
 exports.help = {
