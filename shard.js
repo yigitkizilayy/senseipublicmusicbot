8872fe2f7e7d9c8df344c7ce7a374392fb085996
Glitch (tola-botum) <none>
2019-08-10 08:40:17 +0000

    👞🚿 Checkpoint ./shard.js:3190412/675 ./package.json:3190412/16 ./bot.js:3190412/2 ./ayarlar.json:3190412/252

---
+++ shard.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client()
+const express = require('express');
+const app = express();
+const http = require('http');
+const scarew = new Discord.ShardingManager('./bot.js', {  //Ana dosyanızın adını buraya yazacaksınız.
+    totalShards: 'auto',
+    token: "NTk5OTI1NTIzMzA4NzQwNjI4.XUgu_A.LUHYOfKMTMH83Nk9uLnEDb1wZQU"// Botun tokenini buraya yazacaksınız.
+});
+
+scarew.spawn(); 
+
+scarew.on('launch', shard => {
+  console.log(`**${shard.id}** ID'sine sahip shard, başarıyla başlatıldı. Müzikler hazır, hehehe! `)
+});
+
+setTimeout(() => {
+    scarew.broadcastEval("process.exit()");
+}, 21600000);
