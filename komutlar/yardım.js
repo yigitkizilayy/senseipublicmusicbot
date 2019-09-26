d51bd5eead6921bfe97f7316be3cc710ed5c1cd9
Glitch (tola-botum) <none>
2019-09-26 15:33:01 +0000

    🗼🍏 Checkpoint ./bot.js:6398029/62 ./ayarlar.json:6398029/38 ./events/ready.js:6398029/5040 ./komutlar/yardım.js:6398029/25

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -6,7 +6,7 @@ exports.run = (client, message, params) => {
   const embedyardim = new Discord.RichEmbed()
   .setThumbnail(client.user.avatarURL)
   .setFooter('PLUSKLY Müsic', client.user.avatarURL) 
-  .setDescription(`**PLUSKLY Müsic Bot Komutları**\n\n:musical_note:\`p!oynat\` = **Yazdığınız şarkıyı çalar.** **(p!o)** \n :musical_note:\`p!geç\` = **Dinlediğiniz Şarkıyı Geçer.** **(p!g)** \n :musical_note:\`p!botçık\` = **Şarkıyı Durdurur Ve Bot Odadan Ayrılır.** **(p!ç)** \n :musical_note:\`p!duraklat\` = **Şarkıyı Durdurur**. **(p!d)** \n :musical_note:\`p!devamet\` = **Durdurulan Şarkı Devam Eder**.**(p!de)** \n :musical_note:\`p!davet\` = **Botun Davet Linkini Atar.**.  \n`)
+  .setDescription(`**PLUSKLY Müsic Bot Komutları**\n\n:musical_note:\`pm!oynat\` = **Yazdığınız şarkıyı çalar.** **(pm!o)** \n :musical_note:\`pm!geç\` = **Dinlediğiniz Şarkıyı Geçer.** **(pm!g)** \n :musical_note:\`pm!botçık\` = **Şarkıyı Durdurur Ve Bot Odadan Ayrılır.** **(pm!ç)** \n :musical_note:\`pm!duraklat\` = **Şarkıyı Durdurur**. **(pm!d)** \n :musical_note:\`pm!devamet\` = **Durdurulan Şarkı Devam Eder**.**(pm!de)** \n :musical_note:\`pm!davet\` = **Botun Davet Linkini Atar.**.  \n`)
   .setColor(0x00ffff)
   .setTimestamp()
  message.channel.send(embedyardim);
@@ -21,7 +21,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'komutlar',
+  name: 'yardım',
   description: 'yardım',
   usage: 'yardım'
 };
